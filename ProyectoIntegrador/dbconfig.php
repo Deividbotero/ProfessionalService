fe39ab5c71d6b064d9bb24af7cfcc3ba832d464c
Deividbotero <126630733+Deividbotero@users.noreply.github.com>
2023-10-26 15:48:58 -0500

    Php union base

---
+++ ProyectoIntegrador/dbconfig.php
@@ -0,0 +1,5 @@
+<?php
+$host = 'localhost';
+$dbname = 'professionalservice';
+$username = "root";
+$password ='';
---
+++ ProyectoIntegrador/index.php
@@ -0,0 +1,143 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE-edge">
+    <meta name="viewport" content="width=device=width, initial-scale=1.0">
+    <title>Professional Service</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <div class="menu container">
+            <a href="#" class="logo">ProfessionalService</a>
+            <input type="checkbox" id="menu">
+            <label for="menu">
+                <img src="Imagenes/menu-btn.png" class="menu-icono" alt="">
+            </label>
+            <nav class="navbar">
+                <ul>
+                    <style>
+                        ul.documents {
+                            display: none;
+                        } 
+                        li:hover ul.documents{
+                            display: block
+                        }
+                        </style>
+                    <ul>
+                        <li>
+                            <a href="#">Tercer Semestre</a>
+                            <ul class="documents">
+                                <li><a href="Documentos/Professional Service APA.pdf" target="_blank">Normas APA</a></li>
+                                <li><a href="Documentos/JIRA.pdf" target="_blank">JIRA</a></li>
+                                <li><a href="Documentos/IEEE 830 JUAN MORENO Y DANIEL ESCOBAR (1).pdf" target="_blank">Normas IEEE 830</a></li>
+                        </li>
+                    </ul>
+                </ul>
+            </nav>
+        </div>
+        <div class="header-content container">
+            <div class="content">
+                <h1>Professional Service lo que necesita tu vida</h1> 
+                <p>
+                    Queremos brindarte la mejor experiencia.
+                </p>
+                <a href="#" class="btn-1">Mas informacion</a>
+                <a href="#" class="btn-1">Contactanos</a>
+            </div>
+            <img src="Imagenes/astronauta.png" alt="">
+        </div>
+    </header>   
+    
+    <main class="Servicios container">
+    <div class="Servicios">
+            <img src="Imagenes/icon1.svg" alt="">
+            <h3></h3>
+            <p>
+                ¿Deseas saber mas sobre nosotros?
+            </p>
+    </div>
+    <div class="Servicios">
+        <img src="Imagenes/icon2.svg" alt="">
+        <h3></h3>
+        <p>
+            !Queremos saber como ha sido tu  experiencia!
+        </p>
+    </div>
+    <div class="Servicios">
+        <img src="Imagenes/icon3.svg" alt="">
+        <h3></h3>
+        <p>
+            ¿Tienes alguna duda?
+        </p>
+</main>
+
+<section class="porfolios container">
+
+    <h2></h2>
+
+    <div class="porfolio">
+
+        <img src="Imagenes/img1.png" alt="">
+        <div class="porfolio-1">
+            <h3>Que queremos para ti</h3>
+            <p>
+                Deseamos que puedas darle solucion a tus problemas laborales brindando tus conocimientos y asi generando tus ingresos.
+            </p>
+            <a href="#" class="btn-2">informacion</a>
+        </div>
+    </div>
+    <div class="porfolio">
+        <div class="porfolio-2">
+            <h3>¿Que le ofrecemos a los clientes?</h3>
+            <p>
+                Ofrecemos una prestacion de servcios para que los clientes
+                puedan satisfacer sus necesidades y darle soluciones a sus problemas bien sea de un tema tecnologico hasta un tema mas complejo
+                como lo es la mecanica.
+            </p>
+            <a href="#" class="btn-2">informacion</a>
+        </div>
+        <img src="Imagenes/img2.png" alt="">
+    </div>
+    <div class="porfolio">
+        <img src="Imagenes/img4.png" alt="">
+        <div class="porfolio-1"> 
+            <h3>Nuestra llegada al mercado</h3>
+            <p>
+               Llegaremos fuertemente ya que a nivel nacional no existe ninguna aplicacion donde brinde oportunidades
+               laborales desde el conocimiento perosnal y profesional.
+            </p>
+            <a href="#" class="btn-2">informacion</a>
+        </div>
+    </div>
+</section>
+<footer>
+    <div class="container">
+        <div class="info">
+            <div class="info-content">
+
+                <div class="info-service">
+                    <h2>Servicios</h2>
+                    <p>
+                        Nuestros servicios seran personalizados a lo que nuestros usuarios soliciten,
+                        tendremos disponibilidad de servicio durante las 24 horas.
+                    </p>
+                    <a href="#" class="btn-1">Informacion</a>
+                </div>
+
+            </div>
+            <hr>
+            <div class="enlaces">
+                <h3>ProfessionalService</h3>
+                <div class="enlace">
+                    <li><a href="#">Telefono</a></li>
+                    <li><a href="#">Juan David Moreno Botero - +57 3123734705</a></li>
+                    <li><a href="#">+57 3232902619 - Daniel Giovanny Escobar Santoyo </a></li>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+</body>
+</html>
